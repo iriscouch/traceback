4599754ab3025cd1e3b94b76a1f27e10fd81d035
Jason Smith (work) <jhs@iriscouch.com>
2011-12-14 15:37:11 +0000

    Some comments about the design

--- traceback.js
+++ traceback.js
@@ -38,6 +38,48 @@ function traceback() {
   return stack.map(function(callsite) { return frame.make(callsite) })
 }
 
+// The design:
+//
+// Unfortunately, only way to access raw stack frames is through one global
+// function callback, Error.prepareStackTrace. See
+// node/deps/v8/src/messages.js. So, despite this module having nothing to do
+// with error handling at all, it must do a lot with the Error system.
+//
+// It won't work to simply set Error.prepareStackTrace and leave it there
+// forever. Other code will want to print normal exceptions, formatted
+// normally. Many people expect an error .stack attribute to be a string, and
+// to be formatted in exactly the right way. This module therefore tries to be
+// as conservative as possible.
+//
+// First, when a stack is requested, the idea is to hook in to
+// prepareStackTrace, use it, then reset things back to normal. As long as the
+// original Error.prepareStackTrace is in place, then people will get their
+// expected .stack formatting. The most typical case is that it is
+// undefined/deleted, and the built-in V8 code mentioned above will run.
+//
+// But, this module itself might throw an exception, leaving prepareStackTrace
+// pointing to the wrong function. That is why `getting_the_stack` is in the
+// module. Before every traceback call, it is set from null to a real object.
+// When our prepareStackTrace runs, it checks that the "error" object it got is
+// identical to getting_the_stack. If they are identical, then, yep, things are
+// working fine. After executing, getting_the_stack is set back to null.
+//
+// If cleanup failed somehow, and then a normal Error is created later, this
+// code will be called again, inappropriately. It will detect this situation
+// because the incoming error object will not be getting_the_stack. To recover,
+// it just restores the correct prepareStackTrace function. But it is still on
+// the hook to return a formatted stack value.
+//
+// If the original prepareStackTrace is an actual function, it can be called to
+// produce the expected .stack value. Unfortunately, the most common situation
+// is that it should be *undefined*, and the built-in V8 formatter is desired.
+// But there is no way to execute that code on the given error object. I
+// checked. It sucks. So the final workaround is to ship a copy of that
+// formatter and use it as a last resort.
+//
+// In any case, the correct Error.prepareStackTrace value will be restored, so
+// subsequent exceptions will get the right behavior.
+
 var getting_the_stack = null
 
 function raw_traceback(begin_func) {
