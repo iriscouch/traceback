72fb2f27918075b367401c9aed71486e739b514e
Jason Smith (work) <jhs@iriscouch.com>
2011-12-14 15:25:37 +0000

    Builtin formatting like v8 does

--- api.js
+++ api.js
@@ -12,4 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module.exports = require('./traceback')
+var API = require('./traceback')
+API.v8 = require('./lib/v8')
+
+module.exports = API
